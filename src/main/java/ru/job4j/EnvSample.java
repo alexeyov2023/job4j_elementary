7d519b04639039fcd7bed0b1de658d9fa47f2e6a
alexeyov2023 <alexey.ovchinnikov@gmail.com>
2023-08-16 18:52:37 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/main/java/ru/job4j/EnvSample.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class EnvSample {
+    public static void main(String[] args) {
+        int age = 18;
+        float length = 78.0332f;
+        boolean baby = true;
+
+        System.out.println("16.08.2023");
+    }
+}
